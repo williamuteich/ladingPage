aa1db6853d190ab28f53e6b816fc38825b271eb6
William <williamuteich14@gmail.com>
2024-11-15 15:51:41 -0300

    config projeto.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from "vite"
+
  
 export default defineConfig({
   plugins: [react()],
+  base: "/gpt-page/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
